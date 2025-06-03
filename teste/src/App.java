50f0c81c984957325dcfc04e9828ea0795be032f
diogomota5 <diogo_mota1990@outlook.com>
2025-06-03 18:26:40 -0300

    modified:   teste/src/App.java

--- teste/src/App.java
+++ teste/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Boas vindas ao alunos DIO");
+        System.out.println("Testanto commit");
     }
   }
