1349f7a807007b2f53e5e089f0d292741008be04
diogomota5 <diogo_mota1990@outlook.com>
2025-06-03 18:40:12 -0300

    modified:   teste/src/App.java

--- teste/src/App.java
+++ teste/src/App.java
@@ -1,6 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Boas vindas ao alunos DIO");
-        System.out.println("Testanto commit");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double n1 = 0 , n2 = 0;
+        
+
+        System.out.print("Entre com o primeiro valor: ");
+        n1 = sc.nextDouble();
+        System.out.print("Entre com o segundo valor: ");
+        n2 = sc.nextDouble();
+        double sum = n1 + n2;
+        System.out.print("A soma é: " + String.format("%.2f", sum));
+
     }
   }
